65362611f1d08a4dac865ffab43f27057ab18262
Rhythm <rhythms1906@gmail.com>
2025-10-19 23:51:27 +0530

    Create first.js
    Learning variables and print statement

---
+++ first.js
@@ -0,0 +1,23 @@
+console.log("Rhythm")
+
+
+const abc = "var1"
+console.log(abc)    // Output = var1
+
+/*
+const abc = "var2"
+console.log(abc)    // Error, value of abc cannot be changed
+*/
+
+let xyz = 12345
+console.log(xyz)    // Output = 12345
+
+xyz = "abc"
+console.log(xyz)    // Output = abc
+
+xyz = 123
+console.log(xyz)    // Output = 123
+
+// Baar baar xyz define karne se xyz ki value update ho rahi hai
+
+console.table([abc,xyz,112,"lmao"])    // print karne ka naya tareeka
